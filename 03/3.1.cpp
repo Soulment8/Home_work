a50f7e757bca24726ba597f28ef3ed9405fc5c0a
Soulment <viktorkalashnikov84625@mail.ru>
2026-01-09 03:33:17 +0300

    README.md

---
+++ 03/3.1.cpp
@@ -0,0 +1,237 @@
+﻿
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+class Calculator
+{
+private:
+    double num1 = 0.0, num2 = 0.0;
+    bool k = true;
+
+    double add()
+    {
+
+        return num1 + num2;
+    }
+
+    double subtract_1_2()
+    {
+        return num1 - num2;
+    }
+
+    double subtract_2_1()
+    {
+        return num2 - num1;
+    }
+
+    double multiply()
+    {
+        return num1 * num2;
+    }
+
+    double divide_1_2()
+    {
+        return num1 / num2;
+    }
+
+    double divide_2_1()
+    {
+        return num2 / num1;
+    }
+
+
+
+public:
+
+    bool set_num1(double num1)
+    {
+
+        cout << " Введите первое число: ";
+        cin >> this->num1; cout << endl;
+
+        if (this->num1 != 0)
+        {
+            return true;
+        }
+        else
+        {
+            cout << " Неверное введение " << endl;
+            set_num1(num1);
+            return false;
+        }
+    }
+
+    //bool set_num1()
+    //{
+    //    if(k == false)  
+    //    {
+    //        cout << " Неверное введение " << endl;
+    //        
+    //    }
+    //    k = false;
+
+    //    cout << " Введите первое число: ";
+    //    cin >> this->num1; cout << endl;
+    //    return this->num1 != 0 ? true : set_num1(), k = true;
+    //}
+
+    bool set_num2(double num2)
+    {
+        if (k == false)
+        {
+            cout << " Неверное введение " << endl;
+
+        }
+        k = false;
+
+        cout << " Введите второе число: ";
+        cin >> this->num2; cout << endl;
+        return this->num2 != 0 ? true : set_num2(num2), k = true;
+    }
+
+
+    // Какой из вариантов set_num лучше?
+
+
+
+
+    void get_print_add()
+    {
+        cout << " num1 + num2 = " << add() << endl;
+    }
+    void get_print_subtract_1_2()
+    {
+        cout << " num1 - num2 = " << subtract_1_2() << endl;
+    }
+    void get_print_subtract_2_1()
+    {
+        cout << " num2 - num1 = " << subtract_2_1() << endl;
+    }
+    void get_print_multiply()
+    {
+        cout << " num1 * num2 = " << multiply() << endl;
+    }
+    void get_print_divide_1_2()
+    {
+        cout << " num1 / num2 = " << divide_1_2() << endl;
+    }
+    void get_print_divide_2_1()
+    {
+        cout << " num2 / num1 = " << divide_2_1() << endl;
+    }
+
+    void get_all_print()
+    {
+        set_num1(num1);
+        set_num2(num2);
+
+        get_print_add();
+        get_print_subtract_1_2();
+        get_print_subtract_2_1();
+        get_print_multiply();
+        get_print_divide_1_2();
+        get_print_divide_2_1();
+
+        cout << endl << "-------------------------------------------------" << endl << endl;
+
+        get_all_print();
+    }
+
+
+    void get_all_print_calc()
+    {
+
+        get_print_add();
+        get_print_subtract_1_2();
+        get_print_subtract_2_1();
+        get_print_multiply();
+        get_print_divide_1_2();
+        get_print_divide_2_1();
+
+    }
+
+
+};
+
+
+
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+
+    Calculator num;
+
+    num.get_all_print();
+    cout << endl << endl;
+
+
+
+    /*
+    int variable = 0;
+    do
+     {
+
+
+         cout << " Выбирите действие: " << endl;
+         cout << " 1. num1 + num2" << endl;
+         cout << " 2. num1 - num2 " << endl;
+         cout << " 3. num2 - num1 " << endl;
+         cout << " 4. num1 * num2 " << endl;
+         cout << " 5. num1 / num2 " << endl;
+         cout << " 6. num2 / num1 " << endl;
+         cout << " 7. Вывести все " << endl;
+         cout << " 8. ВЫХОД! " << endl << endl;
+
+         cout << " Ваш вариант: ";
+         cin >> variable; cout << endl;
+
+         num.set_num1();
+         num.set_num2();
+
+     switch (variable)
+     {
+     case(1):
+         num.get_print_add();
+         break;
+
+     case(2):
+         num.get_print_subtract_1_2();
+         break;
+
+     case(3):
+         num.get_print_subtract_2_1();
+         break;
+
+     case(4):
+         num.get_print_multiply();
+         break;
+
+     case(5):
+         num.get_print_divide_1_2();
+         break;
+
+     case(6):
+         num.get_print_divide_2_1();
+         break;
+
+     case(7):
+         num.get_all_print_calc();
+         break;
+
+     case(8):
+         cout << " Вы произвели выход! " << endl;
+         break;
+     }
+
+     cout << endl << "-------------------------------------------------" << endl << endl;
+
+     } while (variable != 8);
+     */
+
+}
+
---
+++ 03/3.2.cpp
@@ -0,0 +1,125 @@
+﻿
+#include <iostream>
+#include <Windows.h>
+#include <limits>
+using namespace std;
+
+class Counter
+{
+private:
+
+    int counter = 0;
+
+    void counter_plus()
+    {
+        counter++;
+    }
+    void counter_minus()
+    {
+        counter--;
+    }
+    void get_counter_print()
+    {
+        cout << " Сейчас ваше значение = " << counter << endl << endl;
+    }
+
+public:
+
+    Counter()
+    {
+
+    }
+
+    Counter(int& n)
+    {
+        counter = n;
+    }
+
+    Counter(string& check_cmd)
+    {
+        counter = 1;
+    }
+
+    void get_menegment_counter(string& check_cmd)
+    {
+        cout << " Введите команду ('+'/'-'/'=' или 'x'): ";
+        cin >> check_cmd; cout << endl;
+
+        if (check_cmd == "+")
+        {
+            counter_plus();
+        }
+        else if (check_cmd == "-")
+        {
+            counter_minus();
+        }
+        else if (check_cmd == "=")
+        {
+            get_counter_print();
+        }
+        else if (check_cmd == "x")
+        {
+            cout << " До свидания! " << endl;
+            return;
+        }
+        else
+        {
+            cout << " Некорректное значение! " << endl;
+        }
+
+        get_menegment_counter(check_cmd);
+    }
+
+};
+
+void check_n(int& n)
+{
+    cout << " Введите начальное занчение счетчика: ";
+    cin >> n;
+
+    while (cin.fail()) {
+        cout << " Некорректное значение! " << endl;
+        cin.clear();
+        cin.ignore(1000, '\n');
+        cout << " Введите начальное занчение счетчика: ";
+        cin >> n;
+    }
+
+    cout << endl;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+
+    string check_cmd;
+
+    cout << " Вы хотите указать начальное значение счетчика? " << endl
+        << " Введите ( yes /no /1(для создания со значением 1) ): "; cin >> check_cmd; cout << endl;
+    if (check_cmd == "yes")
+    {
+        int n = 0;
+        check_n(n);
+        Counter count(n);
+        count.get_menegment_counter(check_cmd);
+    }
+    else if (check_cmd == "no")
+    {
+        Counter count;
+        count.get_menegment_counter(check_cmd);
+    }
+    else if (check_cmd == "1")
+    {
+        Counter count(check_cmd);
+        count.get_menegment_counter(check_cmd);
+    }
+    else
+    {
+        cout << " Некорректное значение! " << endl << endl;
+        main();
+    }
+
+}
+
